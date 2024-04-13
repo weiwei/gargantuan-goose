4c18b0c238050ebcddaa7997e7c6265b365816f8
Weiwei <gastlygem@gmail.com>
2024-04-13 10:29:18 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), db()]
+});
